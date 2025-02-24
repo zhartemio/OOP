d2a279802bcbed60b27051256e5c4883d284adb6
zhart <zigimontartem@gmail.com>
2025-02-24 21:23:27 +0300

    Initial commit

--- Laba1OOP/MainWindow.xaml.cs
+++ Laba1OOP/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,5 +21,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+       
     }
+
 }
+
---
+++ Laba1OOP/Shape.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Windows.Controls;
+
+public abstract class Shape
+{
+	public abstract void Draw(Canvas canvas);
+}
